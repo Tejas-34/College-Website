6b0f5519232ad840e4161646f1e7d555a5e58245
Tejas Patare <117996001+Tejas-34@users.noreply.github.com>
2024-05-27 18:44:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask( __name__, template_folder='templates')
+app = Flask( __name__)
 
-@app.route('/')
+@app.route("/")
 def index():
   return render_template('index.html')
-app.run()
+
+if(__name__ == '__main__'):
+  app.run(host='0.0.0.0',debug=True)
